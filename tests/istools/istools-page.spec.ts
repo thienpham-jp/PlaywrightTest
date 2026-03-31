3694351784effd5f7678a8cab044a8c8e6405b4d
Thien Pham <thienpham.jp@interspace.vn>
2026-03-31 23:48:17 +0700

    commit istool page login

---
+++ tests/istools/istools-page.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+import { IstoolsPage } from '../../pages/istools-page';
+import testData from './istoolsTestData.json';
+
+test.describe('Istools Tests', () => {
+  let istoolsPage: IstoolsPage;
+
+  test.beforeEach(async ({ page }) => {
+    istoolsPage = new IstoolsPage(page);
+    await istoolsPage.navigate();
+  });
+
+  for (const data of testData) {
+    test(data.label, async ({ page }) => {
+      await istoolsPage.login(data.username, data.password);
+
+      await expect(page).toHaveURL(data.expectedUrl);
+
+      if (data.expectedError) {
+        const errorMessage = await istoolsPage.getErrorMessage();
+        expect(errorMessage).toContain(data.expectedError);
+      }
+    });
+  }
+
+  test.afterEach(async ({ page }) => {
+    if (page) {
+      await page.close();
+    }
+  });
+});
