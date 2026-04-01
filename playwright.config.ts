617f8c0550d9bd67fa1639e56a3d7b2866030df2
Thien Pham <thienpham.jp@interspace.vn>
2026-04-01 10:12:07 +0700

    save config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -22,25 +22,25 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [["html", { open: "always" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on',
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
+    trace: "on",
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
     headless: true,
     launchOptions: {
       args: [
-        '--start-maximized',
-        '--disable-features=PasswordLeakDetection',
-        '--no-sandbox',
-        '--disable-dev-shm-usage',
-        '--disable-features=BlockInsecurePrivateNetworkRequests',
-        '--allow-insecure-localhost',
+        "--start-maximized",
+        "--disable-features=PasswordLeakDetection",
+        "--no-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-features=BlockInsecurePrivateNetworkRequests",
+        "--allow-insecure-localhost",
       ],
     },
     viewport: null,
@@ -49,8 +49,8 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { channel: 'chrome' },
+      name: "chromium",
+      use: { channel: "chrome" },
     },
 
     // {
