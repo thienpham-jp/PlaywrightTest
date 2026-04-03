eb8c6dd13a7515faa8223e29d5f3a3d364340b5a
Thien Pham <thienpham.jp@interspace.vn>
2026-04-03 09:18:00 +0700

    change config

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ import { defineConfig, devices } from "@playwright/test";
  */
 export default defineConfig({
   testDir: "./tests",
-  // testIgnore: ["**/publisher/**", "**/istools/**"],
+  testIgnore: ["**/publisher/**", "**/istools/**"],
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
