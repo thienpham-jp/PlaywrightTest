750bed63760da08f19880ddd476d47d7743c7bcc
Thien Pham <thienpham.jp@interspace.vn>
2026-05-28 16:48:35 +0700

    Update reporter configuration to always open HTML report

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,10 +29,7 @@ export default defineConfig({
   /* Increase test timeout for complex forms */
   timeout: 120000,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ["html", { open: "never" }],
-    ["allure-playwright", { outputFolder: "allure-results" }],
-  ],
+  reporter: [["html", { open: "always" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
